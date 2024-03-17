94840f65d16110bd5a37f5ccf558829d15f431e0
lanthian <lanthian@student.unimelb.edu.au>
2024-03-17 00:00:15 +0000

    tetrominoes() generation fixed. 76 possible Place actions now.

--- search/tetrominoes.py
+++ search/tetrominoes.py
@@ -5,6 +5,12 @@ from core import Coord, Direction
 # todo - change to .core for use in program.py perhaps?
 
 def tetrominoes(c: Coord) -> list[Coord]:
+    """Takes a Coord c and generates a list of Place actions of all unique 4 
+    tiled "tetrominoes" that include this coordinate, relating to the board size
+    defined in core.py. List should always be of length 76.
+    (z*2 + s*2 + i*2 + t*4 + o*1 + j*4 + l*4) * 4 = 19 * 4 = 76
+    """
+    # todo- insert into sorted lists rather than inserting and sorting each time
     t = []
 
     in_progress = [[c]]
@@ -12,6 +18,7 @@ def tetrominoes(c: Coord) -> list[Coord]:
     # Work through a queue of semi-assembled tetrominoes to build whole set
     while len(in_progress) > 0:
         curr = in_progress.pop(0)
+        curr.sort()
         # Skip node if already seen
         if curr in seen:
             continue
@@ -27,12 +34,15 @@ def tetrominoes(c: Coord) -> list[Coord]:
         # Loop through each direction for each piece, attaching a new block
         for coord in curr:
             for dir in [d.value for d in Direction]:
-                in_progress.append(curr + [Coord.__add__(coord, dir)])
+                new = Coord.__add__(coord, dir)
+                # Don't duplicate already existing coords in tetromino
+                if new in curr: continue
+                in_progress.append(curr + [new])
 
     return t
 
-
-possible = tetrominoes(Coord(0,0))
-print(len(possible))
-#312 possible tetrominoes placeable around (0,0)? Seems unlikely 
-# - investigate this number
+# possible = tetrominoes(Coord(0,0))
+# possible.sort()
+# for p in possible:
+#     print(p.__str__())
+# print(len(possible))
